f23069338206897001c1f3ffadad8cebb3e02649
Okan Keskin <okannkeskn@gmail.com>
2021-12-16 23:26:19 +0300

    Ufak denemeler

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("button");
+var section = document.getElementById("section");
+
+
+function deneme(){
+    section.style.display = "block";
+    button.style.display = "none";
+}
