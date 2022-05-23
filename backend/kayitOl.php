8f63bab90e90104c199ea023653e73592b875904
Okan Keskin <okannkeskn@gmail.com>
2022-05-23 15:23:21 +0300

    Kayıt ol güncellendi
    Kayıt ol güncellendi

--- backend/kayitOl.php
+++ backend/kayitOl.php
@@ -1,5 +1,17 @@
 <?php
-include './dbbaglan.php';
+$sn = "localhost"; 
+$un = "root";  
+$ps = ""; 
+$db = "rentasoha"; 
+try{
+    $db = new PDO("mysql:host=$sn;dbname=$db",$un,$ps);
+   
+    $db->exec("SET CHARSET UTF8");
+    
+}
+catch(PDOException $e){
+    die ("Hata var");
+}
 include './functions.php';
 
 if(isset($_POST["gonder"])){
@@ -13,18 +25,30 @@ if(isset($_POST["gonder"])){
     $sifreT = temizle($_POST["tsifre"]);
 }
 
+    $query  = $db->query("SELECT * FROM kullanici WHERE eposta='$eposta'",PDO::FETCH_ASSOC);
+   
+    
+
 if($sifre == $sifreT){
-    $sifre = $sifre . "<585asffqxx*pll<<pqo>q</";
-    $sifre = hash('sha512',$sifre);
-    $dogumTar = date("Y-m-d", strtotime($dogumTar));  
-    $sql = "INSERT INTO kullanici (adi, soyadi, tc_no,dogum_tar,cep_tel,eposta,sifre,rol_id)
-    VALUES (?, ?, ?,?,?,?,?,?)";
-    $stmt = $conn->prepare($sql);
-    $rol_id = 0;
-    $stmt->bind_param("sssssssi",$ad, $soyad, $tc,$dogumTar,$ceptel,$eposta,$sifre,$rol_id);
-    $stmt->execute();
-    if($stmt ==TRUE){
-        include '../uyeOlBasarili.html';
-        header("Refresh: 5; url=../Giris.html");
-    }
+    $say = $query -> rowCount();
+
+        if( $say > 0 ){
+            echo "Daha önce kayıt yaptınız.";
+            
+        }
+        else{
+            $sifre = $sifre . "<585asffqxx*pll<<pqo>q</";
+            $sifre = hash('sha512',$sifre);
+            $dogumTar = date("Y-m-d", strtotime($dogumTar));  
+            $sql = "INSERT INTO kullanici (adi, soyadi, tc_no,dogum_tar,cep_tel,eposta,sifre,rol_id)
+            VALUES (?, ?, ?,?,?,?,?,?)";
+            $stmt = $db->prepare($sql);
+            $rol_id = 0;
+            //$stmt->bind_param("sssssssi",$ad, $soyad, $tc,$dogumTar,$ceptel,$eposta,$sifre,$rol_id);
+            $stmt->execute(array($ad, $soyad, $tc,$dogumTar,$ceptel,$eposta,$sifre,$rol_id));
+            if($stmt ==TRUE){
+                include '../uyeOlBasarili.html';
+                header("Refresh: 5; url=../Giris.html");
+            }
+        }
 }
