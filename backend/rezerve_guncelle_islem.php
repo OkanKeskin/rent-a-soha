ee589837447f7fe33c5a666c15b7a189bfca0287
Sayithan <95936713+sayithanxus@users.noreply.github.com>
2022-05-25 02:56:47 +0300

    rezerve_guncelle bitttii

--- backend/rezerve_guncelle_islem.php
+++ backend/rezerve_guncelle_islem.php
@@ -17,18 +17,30 @@ if(isset($_POST["teslim_alma_ofisi"])){
 
 
         $conn->set_charset("utf8");
+        
         $tao=$_POST["teslim_alma_ofisi"];
         $teo=$_POST["teslim_etme_ofisi"];
-        $tat=$_POST["teslim_alma_tarihi"];
-        $tet=$_POST["teslim_etme_tarihi"];
-    
-        $sql  = "update rezervasyon set";
+        $tat=date("Y-m-d", strtotime($_POST["teslim_alma_tarihi"]));  
+        $tet=date("Y-m-d", strtotime($_POST["teslim_etme_tarihi"]));  
+        $tas=date("H:i:s", strtotime($_POST["teslim_alma_saati"])); 
+        $tes=date("H:i:s", strtotime($_POST["teslim_etme_saati"]));
+        $rezid=$_SESSION["rez"];
+
+
+        $sql  = "UPDATE rezervasyon SET teslim_alma_ofisi='".$tao."',teslim_etme_ofisi='".$teo."',rez_tar='".$tat."',teslim_etme_tarihi='".$tet."',teslim_alma_saati='".$tas."',teslim_etme_saati='".$tes."',rezervasyon_guncelleme_tarihi=DATE(NOW()) WHERE rez_id=".$rezid." ";
 
         $result = $conn -> query($sql);
 
+        if($result==true){
+            include '../rezerve_basarili.html';
+            header("Refresh: 2; url=../rezervasyonlarim.php");
+        }
+        else{
+            include '../hatali_bilgi.html';
+    header("Refresh: 2; url=../rezervasyonlarim.php");
+        }
         
     
-        $row=$result->fetch_assoc();
 
            
         
